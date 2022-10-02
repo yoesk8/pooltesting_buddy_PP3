cef4696bef3735cf9085fcbf0fa5b47083a19fef
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-10-02 13:54:28 +0000

    Created register template

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -15,11 +16,17 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_readings")
 def get_readings():
     readings = mongo.db.readings.find()
     return render_template("readings.html", readings=readings)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
