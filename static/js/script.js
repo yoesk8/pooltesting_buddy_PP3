35ec74dcc67a894266d2a85b30d3db9c156ef7e6
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-10-04 11:51:44 +0000

    Added fields to form

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
     $('select').formSelect();
+    $(".datepicker").datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+          done: "Select"
+      }
+  });
+    $('.timepicker').timepicker();
   });
